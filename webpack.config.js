c027f6f963764cc7ed26e8c63295034ecb180fd1
emant <joshua999x@gmail.com>
2020-03-22 00:15:52 +0100

    add build tag

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 const path = require('path');
+const { execSync } = require('child_process');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { version } = require('./package.json');
 
+const getBuildTag = () => {
+  const time = new Date().toISOString();
+  try {
+    const hash = execSync('git rev-parse --short HEAD')
+      .toString()
+      .replace('\n', '');
+    return `${version}-${hash}:${time}`;
+  } catch (e) {
+    console.log(e);
+
+    return `${version}:${time}`;
+  }
+};
 const isDev = mode => mode === 'development';
 const mode = process.env.NODE_ENV || 'development';
 
@@ -57,9 +72,12 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: 'src/index.ejs',
       inject: 'body',
-      hash: true
+      hash: true,
+      templateParameters: {
+        build: getBuildTag()
+      }
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
