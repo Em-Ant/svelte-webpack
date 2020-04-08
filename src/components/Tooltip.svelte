d130348a0bb90bb8019a30b1b9c45beb1a55534f
emant <joshua999x@gmail.com>
2020-04-08 17:42:04 +0200

    tooltip

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,139 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { fade } from "svelte/transition";
+  import { createPopper } from "@popperjs/core";
+
+  export let attrs = {};
+  export let options = {};
+  export let ref = null;
+
+  let tooltip;
+  let popper;
+
+  const defaults = {
+    placement: "right",
+    modifiers: [
+      {
+        name: "offset",
+        options: {
+          offset: [4, 10]
+        }
+      },
+      {
+        name: "preventOverflow",
+        options: {
+          altAxis: true // false by default
+        }
+      },
+      {
+        name: "arrow",
+        options: {
+          padding: 4
+        }
+      },
+      {
+        name: "flip",
+        options: {
+          padding: 4,
+          fallbackPlacements: [
+            "right-end",
+            "right-start",
+            "bottom",
+            "bottom-end",
+            "top",
+            "top-end",
+            "left-start",
+            "left-end",
+            "left"
+          ],
+          flipVariations: false
+        }
+      }
+    ]
+  };
+
+  $: if (ref && options) {
+    destroy();
+    create();
+  }
+
+  function create() {
+    if (ref && tooltip && !popper) {
+      popper = createPopper(ref, tooltip, { ...defaults, options });
+    }
+  }
+  function destroy() {
+    if (popper) {
+      popper.destroy();
+      popper = null;
+    }
+  }
+  onMount(function() {
+    create();
+  });
+  onDestroy(function() {
+    destroy();
+  });
+</script>
+
+<style>
+  div.tooltip {
+    background-color: #282828;
+    color: white;
+    width: 180px;
+    min-height: 40px;
+    display: flex;
+    align-items: center;
+    border-radius: 2px;
+    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.4);
+    opacity: 1;
+    transition: opacity 0.2s cubic-bezier(0.445, 0.05, 0.55, 0.95);
+    font-size: 13px;
+    font-weight: normal;
+    padding: 8px;
+    z-index: 1;
+  }
+  div.arrow,
+  div.arrow::before {
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    z-index: -1;
+  }
+
+  div.arrow::before {
+    content: "";
+    transform: rotate(45deg);
+    background: #282828;
+  }
+  :global(div.tooltip[data-popper-reference-hidden]) {
+    opacity: 0 !important;
+    pointer-events: none;
+  }
+  :global(div.tooltip[data-popper-placement^="top"] > div.arrow) {
+    bottom: -4px;
+  }
+
+  :global(div.tooltip[data-popper-placement^="bottom"] > div.arrow) {
+    top: -4px;
+  }
+
+  :global(div.tooltip[data-popper-placement^="left"] > div.arrow) {
+    right: -4px;
+  }
+
+  :global(div.tooltip[data-popper-placement^="right"] > div.arrow) {
+    left: -4px;
+  }
+</style>
+
+{#if ref}
+  <div
+    {attrs}
+    transition:fade={{ duration: 150 }}
+    class="tooltip"
+    bind:this={tooltip}>
+    <div data-popper-arrow class="arrow" />
+    <slot />
+  </div>
+{/if}
